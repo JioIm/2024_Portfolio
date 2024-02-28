dba5caf04bca1d5a735e09e982eaf391e994ed87
Jio Im <148739315+JioIm@users.noreply.github.com>
2024-02-28 18:17:25 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,47 @@
+$(function () {
+
+    $('#main').fullpage({
+        anchors: ["p1", "p2", "p3", "p4", "p5"],
+        menu: '#custom_nav',
+        scrollOverflow: false,
+        navigation: true,
+        paddingTop: '40px',
+
+        afterRender: () => {
+            mainVisualLine();
+        },
+
+        afterLoad: function (origin, destination, direction, trigger) {
+            //destination.index === 0 && mainVisualLine();
+            if (destination.index === 0) {
+                $('.f_wrap').addClass('on');
+
+            } else {
+                $('.f_wrap').removeClass('on')
+            }
+        },
+
+        onLeave: function (origin, destination, direction, trigger) {
+            destination.index === 0 && mainVisualLine();
+        },
+
+
+        responsiveWidth: 768,
+        responsiveHeight: 800,
+
+    });
+});
+
+
+
+
 function mainVisualLine() {
-    gsap.to('.mainVisual .tit p em', {
-        width: 600,
+    const m = document.querySelector('.mainVisual .tit p em')
+    const tl = gsap.timeline();
+    tl.from(m, {
+        width: 0,
         duration: 2,
         delay: 1
     })
-
-    gsap.to(".animate-anything-css", {
-        duration: 10,
-        ease: "none",
-        repeat: -1,
-        rotation: 360,
-    })
 }
 
-mainVisualLine()
